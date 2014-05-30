fcfc461b8b1e516b03d55dae04dd77483a97465b
jhuriez <syntaxlb@gmail.com>
2014-05-30 15:26:22 +0200

    [UPDATE] Use panel element boostrap

--- views/backend/add.php
+++ views/backend/add.php
@@ -1,46 +1,60 @@
 <?= \Form::open(array('role' => 'form', 'class' => 'form-horizontal')); ?>
 <div class="row">
     <div class="col-lg-6">
-      <div class=" form-group">
-        <label id="label_slug" for="form_slug" class="control-label col-lg-2"><?= __('url_model_url.slug'); ?></label>
-        <div class="col-lg-10">
-          <div class="input-group">
-            <span class="input-group-btn">
-              <button class="btn btn-warning random" type="button" data-target="#form_slug"><?= __('url.random'); ?></button>
-            </span>
-            <input type="text" class="form-control" id="form_slug" name="slug" value="<?= $form->field('slug')->get_attribute('value'); ?>"> <span></span> 
-          </div><!-- /input-group -->
+      <section class="panel panel-default">
+        <div class="panel-heading">
+          <h3>Configuration</h3>
         </div>
-      </div>
 
-      <?= $form->field('url_target')->set_attribute(array('class' => 'form-control')); ?>
-      <?= $form->field('code')->set_attribute(array('class' => 'form-control')); ?>
-      <?= $form->field('method')->set_attribute(array('class' => 'form-control')); ?>
-      <?= $form->field('description')->set_attribute(array('class' => 'form-control')); ?>
-      <?= $form->field('active')->set_attribute(array('class' => 'form-control')); ?>
-      <?= $form->field('add'); ?>
+        <div class="panel-body">
+          <div class=" form-group">
+            <label id="label_slug" for="form_slug" class="control-label col-lg-2"><?= __('url_model_url.slug'); ?></label>
+            <div class="col-lg-10">
+              <div class="input-group">
+                <span class="input-group-btn">
+                  <button class="btn btn-warning random" type="button" data-target="#form_slug"><?= __('url.random'); ?></button>
+                </span>
+                <input type="text" class="form-control" id="form_slug" name="slug" value="<?= $form->field('slug')->get_attribute('value'); ?>"> <span></span> 
+              </div><!-- /input-group -->
+            </div>
+          </div>
+
+          <?= $form->field('url_target')->set_attribute(array('class' => 'form-control')); ?>
+          <?= $form->field('code')->set_attribute(array('class' => 'form-control')); ?>
+          <?= $form->field('method')->set_attribute(array('class' => 'form-control')); ?>
+          <?= $form->field('description')->set_attribute(array('class' => 'form-control')); ?>
+          <?= $form->field('active')->set_attribute(array('class' => 'form-control')); ?>
+          <?= $form->field('add'); ?>
+        </div>
+      </section>
     </div>
     <div class="col-lg-6">
       <?php if(!empty($url->associated_urls)): ?>
-      <h2><?= __('url.title.associated_url'); ?> : </h2>
-      <div class="well associated-url">
-        <dl class="dl-horizontal">
-          <?php foreach($url->associated_urls as $associated_url): ?>
-            <div>  
-            <dt><?= $associated_url->slug; ?></dt>
-            <dd>
-              <?php if($associated_url->active): ?>
-                <a href="#" class="toggle activate" data-id="<?= $associated_url->id; ?>"><i class="fa fa-check-square-o fa-lg"></i></a>
-              <?php else: ?>
-                <a href="#" class="toggle desactivate" data-id="<?= $associated_url->id; ?>"><i class="fa fa-square-o fa-lg"></i></a>
-              <?php endif; ?>       
-              <a href="#" class="delete" data-id="<?= $associated_url->id; ?>"><i class="fa fa-trash-o fa-lg"></i></a>
+        <section class="panel panel-default">
+          <div class="panel-heading">
+            <h3><?= __('url.title.associated_url'); ?></h3>
+          </div>
+          <div class="panel-body">
+            <div class="associated-url">
+              <dl class="dl-horizontal">
+                <?php foreach($url->associated_urls as $associated_url): ?>
+                  <div>  
+                  <dt><?= $associated_url->slug; ?></dt>
+                  <dd>
+                    <?php if($associated_url->active): ?>
+                      <a href="#" class="toggle activate" data-id="<?= $associated_url->id; ?>"><i class="fa fa-check-square-o fa-lg"></i></a>
+                    <?php else: ?>
+                      <a href="#" class="toggle desactivate" data-id="<?= $associated_url->id; ?>"><i class="fa fa-square-o fa-lg"></i></a>
+                    <?php endif; ?>       
+                    <a href="#" class="delete" data-id="<?= $associated_url->id; ?>"><i class="fa fa-trash-o fa-lg"></i></a>
 
-            </dd>
+                  </dd>
+                  </div>
+                <?php endforeach; ?>
+              </dl>
             </div>
-          <?php endforeach; ?>
-        </dl>
-      </div>
+          </div>
+        </section>
       <?php endif; ?>
     </div>
 </div>
--- views/backend/widget.php
+++ views/backend/widget.php
@@ -1,15 +1,19 @@
 <?= \Form::open(array('action' => \Router::get('url_create_short'), 'method' => 'get')); ?>
 	<div class="row">
 	  <div class="col-lg-6">
-	  	<div class="well">
-	  	<h2><?= __('url.url_shortener'); ?></h2>
-	    <div class="input-group">
-	      <input type="text" name="url" class="form-control" placeholder="<?= __('url.your_url'); ?>">
-	      <span class="input-group-btn">
-	        <input class="btn btn-primary" name="add" type="submit" value="<?= __('url.create_short_url'); ?>"></input>
-	      </span>
-	    </div><!-- /input-group -->
-	  	</div>
+		<section class="panel panel-default">
+			<div class="panel-heading">
+			  	<strong><?= __('url.url_shortener'); ?></strong>
+			</div>
+			<div class="panel-body">
+			    <div class="input-group">
+			      <input type="text" name="url" class="form-control" placeholder="<?= __('url.your_url'); ?>">
+			      <span class="input-group-btn">
+			        <input class="btn btn-primary" name="add" type="submit" value="<?= __('url.create_short_url'); ?>"></input>
+			      </span>
+			    </div><!-- /input-group -->
+			</div>
+		</section>
 	  </div><!-- /.col-lg-6 -->
 	</div><!-- /.row -->
 <?php \Form::close(); ?>
